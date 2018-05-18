adb9c0f3272b9fe2f9ee37fdafc2da26ca7a7fd9
graemerocher <graeme.rocher@gmail.com>
2018-05-18 11:44:03 +0200

    Fix Kotlin example

--- hello-world-kotlin/src/main/kotlin/example/Application.kt
+++ hello-world-kotlin/src/main/kotlin/example/Application.kt
@@ -28,8 +28,10 @@ object Application {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        System.setProperty("org.slf4j.simpleLogger", "TRACE")
-        Micronaut.build().packages(Package.getPackage("example")).start(javaClass)
+        Micronaut.build()
+                 .packages("example")
+                 .mainClass(Application.javaClass)
+                 .start()
     }
 }
 

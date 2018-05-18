3fc75d7777b8286de1f69e173166dbe8cb31d468
graemerocher <graeme.rocher@gmail.com>
2018-05-18 12:08:01 +0200

    Add android example

---
+++ android-example/app/src/main/java/demo/micronautdiandroid/Book.java
@@ -0,0 +1,17 @@
+package demo.micronautdiandroid;
+
+public class Book {
+    String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}

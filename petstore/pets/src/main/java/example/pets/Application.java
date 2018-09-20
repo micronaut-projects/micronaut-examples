a2afb981189ab8737bebbb2bcd561efb5ab3c738
Graeme Rocher <graeme.rocher@gmail.com>
2018-09-20 15:16:38 +0200

    add test for non-reactive types

--- petstore/pets/src/main/java/example/pets/Application.java
+++ petstore/pets/src/main/java/example/pets/Application.java
@@ -24,6 +24,40 @@ import javax.inject.Singleton;
  * @author graemerocher
  * @since 1.0
  */
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.*;
+import io.swagger.v3.oas.annotations.tags.*;
+import io.swagger.v3.oas.annotations.servers.*;
+import io.swagger.v3.oas.annotations.security.*;
+
+@OpenAPIDefinition(
+    info = @Info(
+            title = "the title",
+            version = "0.0",
+            description = "My API",
+            license = @License(name = "Apache 2.0", url = "http://foo.bar"),
+            contact = @Contact(url = "http://gigantic-server.com", name = "Fred", email = "Fred@gigagantic-server.com")
+    ),
+    tags = {
+            @Tag(name = "Tag 1", description = "desc 1", externalDocs = @ExternalDocumentation(description = "docs desc")),
+            @Tag(name = "Tag 2", description = "desc 2", externalDocs = @ExternalDocumentation(description = "docs desc 2")),
+            @Tag(name = "Tag 3")
+    },
+    externalDocs = @ExternalDocumentation(description = "definition docs desc"),
+    security = {
+            @SecurityRequirement(name = "req 1", scopes = {"a", "b"}),
+            @SecurityRequirement(name = "req 2", scopes = {"b", "c"})
+    },
+    servers = {
+            @Server(
+                    description = "server 1",
+                    url = "http://foo",
+                    variables = {
+                            @ServerVariable(name = "var1", description = "var 1", defaultValue = "1", allowableValues = {"1", "2"}),
+                            @ServerVariable(name = "var2", description = "var 2", defaultValue = "1", allowableValues = {"1", "2"})
+                    })
+    }
+)
 @Singleton
 public class Application {
 

a6baa52aeb50ff73d91a1cfe05a70520a555031e
Rostislav Svoboda <rsvoboda@redhat.com>
2018-12-11 16:51:44 +0100

    [#31] Return content-type: text/plain for hello-world-java

--- hello-world-java/src/main/java/example/HelloController.java
+++ hello-world-java/src/main/java/example/HelloController.java
@@ -15,6 +15,7 @@
  */
 package example;
 
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.validation.Validated;
@@ -30,7 +31,7 @@ import javax.validation.constraints.NotBlank;
 @Validated
 public class HelloController {
 
-    @Get("/hello/{name}")
+    @Get(uri = "/hello/{name}", produces = MediaType.TEXT_PLAIN)
     public Single<String> hello(@NotBlank String name) {
         return Single.just("Hello " + name + "!");
     }

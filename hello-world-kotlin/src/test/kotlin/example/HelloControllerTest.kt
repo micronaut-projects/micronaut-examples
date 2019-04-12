c1613f5c978ce3920a9f2ad76ae9dc05b3295f11
Iván López <lopez.ivan@gmail.com>
2019-04-12 16:05:59 +0200

    Upgrade hello-world-kotlin to Micronaut 1.1.0

--- hello-world-kotlin/src/test/kotlin/example/HelloControllerTest.kt
+++ hello-world-kotlin/src/test/kotlin/example/HelloControllerTest.kt
@@ -1,16 +1,15 @@
 package example
 
 import io.micronaut.test.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
 import javax.inject.Inject
-import  org.junit.jupiter.api.Assertions.*
 
 @MicronautTest
 class HelloControllerTest {
 
     @Inject
-    lateinit var helloClient : HelloClient
+    lateinit var helloClient: HelloClient
 
     @Test
     fun testGreetingService() {

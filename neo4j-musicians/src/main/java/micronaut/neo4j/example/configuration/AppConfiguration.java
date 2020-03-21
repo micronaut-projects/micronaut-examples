18902e9d5dbcd2b1ecd0d3f0bc379a03c89080bf
Jaime Blanco Puga <jaimeblancopuga@gmail.com>
2020-03-21 15:37:07 +0100

    Use @Singleton instead @Bean

--- neo4j-musicians/src/main/java/micronaut/neo4j/example/configuration/AppConfiguration.java
+++ neo4j-musicians/src/main/java/micronaut/neo4j/example/configuration/AppConfiguration.java
@@ -1,19 +1,20 @@
 package micronaut.neo4j.example.configuration;
 
-import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
 import org.neo4j.ogm.config.Configuration;
 import org.neo4j.ogm.session.SessionFactory;
 
+import javax.inject.Singleton;
+
 @Factory
 public class AppConfiguration {
 
-    @Bean
+    @Singleton
     public Configuration neo4jConfig() {
         return new Configuration.Builder().build();
     }
 
-    @Bean
+    @Singleton
     public SessionFactory sessionFactory(Configuration conf) {
         return new SessionFactory(conf, "micronaut.neo4j.example.domain");
     }
